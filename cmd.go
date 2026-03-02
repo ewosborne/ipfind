b40908812b3ea0df2451ee2b4601b188cb4048d3
Eric Osborne <13036248+ewosborne@users.noreply.github.com>
2026-03-01 20:41:40 -0500

    wip ok

--- cmd.go
+++ cmd.go
@@ -3,172 +3,159 @@ package main
 /* this is the main command loop.  it figures out what files need to be parsed and calls
    parseFile for each one.  concurrency-ready but 1 worker to start.
 */
-import (
-	"bufio"
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/fs"
-	"os"
-	"path/filepath"
-	"slices"
 
-	"github.com/charmbracelet/log"
-	"github.com/seancfoley/ipaddress-go/ipaddr"
-)
+// type inputFile struct {
+// 	Filename string
+// 	IsStdin  bool
+// 	Scanner  *bufio.Scanner
+// }
 
-type inputFile struct {
-	Filename string
-	IsStdin  bool
-	Scanner  *bufio.Scanner
-}
+// func displayOutput(w io.Writer, args cliArgStruct, matchedLines []dataMatch, ipv4Trie ipaddr.IPv4AddressTrie, ipv6Trie ipaddr.IPv6AddressTrie) {
 
-func displayOutput(w io.Writer, args cliArgStruct, matchedLines []dataMatch, ipv4Trie ipaddr.IPv4AddressTrie, ipv6Trie ipaddr.IPv6AddressTrie) {
+// 	// three output formats: text, json, trie
 
-	// three output formats: text, json, trie
+// 	if args.Json {
+// 		b, err := json.MarshalIndent(matchedLines, "", "  ")
+// 		if err != nil {
+// 			log.Error(err)
+// 		}
+// 		fmt.Fprint(w, string(b))
+// 	} else if args.Trie {
+// 		if ipv4Trie.Size() > 0 {
+// 			fmt.Fprintln(w, ipv4Trie)
+// 		}
 
-	if args.Json {
-		b, err := json.MarshalIndent(matchedLines, "", "  ")
-		if err != nil {
-			log.Error(err)
-		}
-		fmt.Fprint(w, string(b))
-	} else if args.Trie {
-		if ipv4Trie.Size() > 0 {
-			fmt.Fprintln(w, ipv4Trie)
-		}
+// 		if ipv6Trie.Size() > 0 {
+// 			fmt.Fprintln(w, ipv6Trie)
+// 		}
+// 	} else { // default mode is per line
+// 		/* OK
+// 		if it's Longest I need to find the longest masklen and print only that
+// 		by setting machedLines to whatever's in that trie.
+// 		otherwise print everything else.
+// 		*/
 
-		if ipv6Trie.Size() > 0 {
-			fmt.Fprintln(w, ipv6Trie)
-		}
-	} else { // default mode is per line
-		/* OK
-		if it's Longest I need to find the longest masklen and print only that
-		by setting machedLines to whatever's in that trie.
-		otherwise print everything else.
-		*/
+// 		if args.Longest {
+// 			tmp := []dataMatch{}
+// 			// find longest mask in the trie
+// 			var longest int
+// 			if ipv4Trie.Size() > 0 {
+// 				iter := ipv4Trie.BlockSizeNodeIterator(true)
+// 				//fmt.Printf("%v\n", ipv4Trie)
+// 				for iter.HasNext() {
+// 					node := iter.Next()
+// 					addr := node.GetKey()
+// 					if addr.IsPrefixed() {
+// 						longest = max(longest, addr.GetNetworkPrefixLen().Len())
 
-		if args.Longest {
-			tmp := []dataMatch{}
-			// find longest mask in the trie
-			var longest int
-			if ipv4Trie.Size() > 0 {
-				iter := ipv4Trie.BlockSizeNodeIterator(true)
-				//fmt.Printf("%v\n", ipv4Trie)
-				for iter.HasNext() {
-					node := iter.Next()
-					addr := node.GetKey()
-					if addr.IsPrefixed() {
-						longest = max(longest, addr.GetNetworkPrefixLen().Len())
+// 					}
+// 				}
+// 			} else {
+// 				iter := ipv6Trie.BlockSizeNodeIterator(true)
+// 				//fmt.Printf("%v\n", ipv4Trie)
+// 				for iter.HasNext() {
+// 					node := iter.Next()
+// 					addr := node.GetKey()
+// 					if addr.IsPrefixed() {
+// 						longest = max(longest, addr.GetNetworkPrefixLen().Len())
 
-					}
-				}
-			} else {
-				iter := ipv6Trie.BlockSizeNodeIterator(true)
-				//fmt.Printf("%v\n", ipv4Trie)
-				for iter.HasNext() {
-					node := iter.Next()
-					addr := node.GetKey()
-					if addr.IsPrefixed() {
-						longest = max(longest, addr.GetNetworkPrefixLen().Len())
+// 					}
+// 				}
+// 			}
+// 			log.Debugf("longest is /%v\n", longest)
+// 			// TODO print only matches with that mask
+// 			for _, m := range matchedLines {
+// 				for _, ip := range m.MatchIPs {
+// 					if ip.GetNetworkPrefixLen().Len() == longest {
+// 						//log.Debugf("%v:%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine, m.MatchIPs)
+// 						//fmt.Fprintf(w, "%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine)
+// 						tmp = append(tmp, m)
+// 					}
+// 				}
+// 			}
+// 			matchedLines = tmp
+// 		}
+// 		for _, m := range matchedLines {
+// 			log.Debugf("%v:%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine, m.MatchIPs)
+// 			fmt.Fprintf(w, "%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine)
+// 		}
+// 	}
+// }
 
-					}
-				}
-			}
-			log.Debugf("longest is /%v\n", longest)
-			// TODO print only matches with that mask
-			for _, m := range matchedLines {
-				for _, ip := range m.MatchIPs {
-					if ip.GetNetworkPrefixLen().Len() == longest {
-						//log.Debugf("%v:%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine, m.MatchIPs)
-						//fmt.Fprintf(w, "%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine)
-						tmp = append(tmp, m)
-					}
-				}
-			}
-			matchedLines = tmp
-		}
-		for _, m := range matchedLines {
-			log.Debugf("%v:%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine, m.MatchIPs)
-			fmt.Fprintf(w, "%v:%v:%v\n", m.Filename, m.Idx, m.MatchLine)
-		}
-	}
-}
+// func ipcmd(w io.Writer, args cliArgStruct) error {
 
-func ipcmd(w io.Writer, args cliArgStruct) error {
+// 	iFiles, err := get_inputFiles(args)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to get input files: %w", err)
+// 	}
 
-	iFiles, err := get_inputFiles(args)
-	if err != nil {
-		return fmt.Errorf("failed to get input files: %w", err)
-	}
+// 	// walk stuff.  this needs a rewrite with channels and a worker pool
+// 	for _, i := range iFiles {
+// 		//fmt.Printf("need to process file %v\n", i.Filename)
+// 		// launch a goroutine per file maybe?  for now just do it in order
 
-	// walk stuff.  this needs a rewrite with channels and a worker pool
-	for _, i := range iFiles {
-		//fmt.Printf("need to process file %v\n", i.Filename)
-		// launch a goroutine per file maybe?  for now just do it in order
+// 		if i.IsStdin {
+// 			i.Scanner = bufio.NewScanner(os.Stdin)
+// 		} else {
+// 			ifh, err := os.Open(i.Filename)
+// 			if err != nil {
+// 				log.Errorf("could not open file %v\n", i.Filename)
+// 			}
+// 			defer ifh.Close()
+// 			i.Scanner = bufio.NewScanner(ifh)
 
-		if i.IsStdin {
-			i.Scanner = bufio.NewScanner(os.Stdin)
-		} else {
-			ifh, err := os.Open(i.Filename)
-			if err != nil {
-				log.Errorf("could not open file %v\n", i.Filename)
-			}
-			defer ifh.Close()
-			i.Scanner = bufio.NewScanner(ifh)
+// 		}
+// 	}
 
-		}
-	}
-
-	return nil
-} // func ipcmd
+// 	return nil
+// } // func ipcmd
 
 // v   keep this function
-func getFilesFromArgs(inputFiles []string) ([]string, error) {
-	var ret []string
-	for _, ifile := range inputFiles {
-		err := filepath.WalkDir(ifile, func(path string, d fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
+// func getFilesFromArgs(inputFiles []string) ([]string, error) {
+// 	var ret []string
+// 	for _, ifile := range inputFiles {
+// 		err := filepath.WalkDir(ifile, func(path string, d fs.DirEntry, err error) error {
+// 			if err != nil {
+// 				return err
+// 			}
 
-			if !d.IsDir() {
-				ret = append(ret, path)
-			}
-			return nil
-		})
-		if err != nil {
-			return nil, err
-		}
-	}
-	return ret, nil
-}
+// 			if !d.IsDir() {
+// 				ret = append(ret, path)
+// 			}
+// 			return nil
+// 		})
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 	}
+// 	return ret, nil
+// }
 
-func get_inputFiles(args cliArgStruct) ([]inputFile, error) {
-	// returns a list of input files
+// func get_inputFiles(args cliArgStruct) ([]inputFile, error) {
+// 	// returns a list of input files
 
-	var iFiles = []inputFile{}
+// 	var iFiles = []inputFile{}
 
-	// build list of files or stdin
-	if len(args.InputFiles) == 0 {
-		log.Debug("reading from stdin")
-		iFiles = append(iFiles, inputFile{IsStdin: true})
-	} else {
-		log.Debug("ifiles are", "ifiles", args.InputFiles)
-		// InputFiles is a slice. each element in the slice is either a file or a directory name.
-		files, err := getFilesFromArgs(args.InputFiles)
-		if err != nil {
-			return nil, err
-		}
+// 	// build list of files or stdin
+// 	if len(args.InputFiles) == 0 {
+// 		log.Debug("reading from stdin")
+// 		iFiles = append(iFiles, inputFile{IsStdin: true})
+// 	} else {
+// 		log.Debug("ifiles are", "ifiles", args.InputFiles)
+// 		// InputFiles is a slice. each element in the slice is either a file or a directory name.
+// 		files, err := getFilesFromArgs(args.InputFiles)
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		slices.Sort(files)
+// 		slices.Sort(files)
 
-		log.Debug("files to walk are", "file", files)
-		for _, file := range files {
-			iFiles = append(iFiles, inputFile{IsStdin: false, Filename: file})
+// 		log.Debug("files to walk are", "file", files)
+// 		for _, file := range files {
+// 			iFiles = append(iFiles, inputFile{IsStdin: false, Filename: file})
 
-		}
-	}
+// 		}
+// 	}
 
-	return iFiles, nil
-}
+// 	return iFiles, nil
+// }
--- newcmd.go
+++ newcmd.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/seancfoley/ipaddress-go/ipaddr"
+)
+
+var (
+	ipv4Regex_withSlash = regexp.MustCompile(`(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}(/\d{1,2}))`)
+	ipv6Regex_withSlash = regexp.MustCompile(`([:0-9a-fA-F]{2,39}(/[0-9]{1,3}))`)
+	ipv4Regex_noSlash   = regexp.MustCompile(`(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}(/\d{1,2})?)`)
+	ipv6Regex_noSlash   = regexp.MustCompile(`([:0-9a-fA-F]{2,39}(/[0-9]{1,3})?)`)
 )
 
 type newInputFile struct {
@@ -70,8 +80,8 @@ func newipcmd(w io.Writer, args cliArgStruct) error {
 					}
 				}
 			case args.Subnet:
-				log.Print("need to match subnet")
-				log.Printf("working on line %v", fLine)
+				log.Debug("need to match subnet")
+				//log.Printf("working on line %v", fLine)
 				for _, ip := range fLine.MatchingIPStrings {
 					ipObj := ipaddr.NewIPAddressString(ip).GetAddress()
 					if args.Ipaddr.Contains(ipObj) {
@@ -95,17 +105,34 @@ func newipcmd(w io.Writer, args cliArgStruct) error {
 			}
 		}
 
+		var matchingLines = []*readLine{}
+		for _, fLine := range fLines {
+			if fLine.IsMatch {
+				matchingLines = append(matchingLines, fLine)
+			}
+		}
+
 		// now do some reporting
 		switch {
 		case args.Json:
-			log.Print("TODO need to log JSON")
+			log.Debug("TODO need to log JSON")
+			b, err := json.MarshalIndent(matchingLines, "", "  ")
+
+			if err != nil {
+				log.Error(err)
+			}
+			fmt.Fprint(w, string(b))
+			fmt.Fprint(w, "\n")
+
 		case args.Trie:
 			log.Print("TODO need to log trie")
+			// create tries and then print them
+			//  also need this for LPM I think.
 		default:
-			log.Print("need to log text")
+			log.Debug("need to log text")
 			for _, fLine := range fLines {
 				if fLine.IsMatch {
-					fmt.Printf("%v:%v\n", fLine.Idx, fLine.Line)
+					fmt.Fprintf(w, "%v:%v\n", fLine.Idx, fLine.Line)
 				}
 			}
 		}
@@ -148,12 +175,37 @@ func readSingleFile(args cliArgStruct, fileName newInputFile) ([]*readLine, erro
 		// now find all ipv4 regex matches and ipv6 regex matches from the line
 		// TODO: only check the regex that
 		if args.IsIPv4 {
-			rl.MatchingIPStrings = args.IPv4Regex.FindAllString(line, -1)
+			rl.MatchingIPStrings = get_ipv4_addresses_from_line(rl.Line, args.IPv4Regex)
 		} else {
-			rl.MatchingIPStrings = args.IPv6Regex.FindAllString(line, -1)
+			rl.MatchingIPStrings = get_ipv6_addresses_from_line(rl.Line, args.IPv4Regex)
 		}
 
 		linesInFile = append(linesInFile, &rl)
 	}
 	return linesInFile, nil
+}
+
+func get_ip_addresses_from_line(ipre *regexp.Regexp, line string) []string {
+	return ipre.FindAllString(line, -1)
+}
+
+// TODO can I do this part in parallel?  ipv6 in particular is expensive.
+// syncMap maybe? ick.
+//
+//	channels?
+func get_ipv4_addresses_from_line(line string, ipv4Regex *regexp.Regexp) []string {
+	return get_ip_addresses_from_line(ipv4Regex, line)
+}
+
+func get_ipv6_addresses_from_line(line string, ipv6Regex *regexp.Regexp) []string {
+
+	// hack because the regex is getting messy but this seems ok.
+	ret := []string{}
+	for _, m := range get_ip_addresses_from_line(ipv6Regex, line) {
+		if strings.Contains(m, ":") {
+			ret = append(ret, m)
+		}
+	}
+	return ret
+
 }
