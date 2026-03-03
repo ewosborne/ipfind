9aa95458dbdb7f1dbd50c3a264ab2735f503268a
Eric Osborne <erico@netflix.com>
2026-03-03 11:32:48 -0500

    cleanup

--- cmd.go
+++ cmd.go
@@ -8,6 +8,8 @@ etc
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"io"
 	"io/fs"
 	"os"
@@ -73,6 +75,7 @@ func ipcmd(w io.Writer, args cliArgStruct) error {
 		var foundLines []*foundLineType // lines that do something
 		var IPv4Trie = ipaddr.IPv4AddressTrie{}
 		var IPv6Trie = ipaddr.IPv6AddressTrie{}
+		var matchedLines []*foundLineType
 
 		log.Info("need to load", "file", fileName)
 		if fileName.IsStdin {
@@ -87,7 +90,8 @@ func ipcmd(w io.Writer, args cliArgStruct) error {
 			fileName.Scanner = bufio.NewScanner(ifh)
 		}
 
-		var foundLine *foundLineType
+		//var foundLine *foundLineType
+		var lineObj *foundLineType
 		for fileName.Scanner.Scan() {
 			idx++ // line numbers start at 1
 			line := fileName.Scanner.Text()
@@ -97,8 +101,8 @@ func ipcmd(w io.Writer, args cliArgStruct) error {
 			// parse the line into its bits and fill out struct
 			ipMatches := get_ip_addresses_from_line(args.IPRegex, args.addressFamily, line)
 			log.Debugf("regex matches are %+v", ipMatches)
-			foundLine = &foundLineType{Idx: idx, ipRegexMatches: ipMatches, Line: line}
-			foundLines = append(foundLines, foundLine)
+			lineObj = &foundLineType{Filename: fileName.Filename, Idx: idx, ipRegexMatches: ipMatches, Line: line}
+			foundLines = append(foundLines, lineObj)
 
 			// now see if there are condtion matches
 			// append each to foundLine.conditionMatches and set foundline.isMatch
@@ -127,254 +131,55 @@ func ipcmd(w io.Writer, args cliArgStruct) error {
 					log.Debug("comparing %v %v for Exact", ipObject, args.Ipaddr)
 					if ipObject.Equal(args.Ipaddr) {
 						log.Debug("found equal match %v", ipObject)
-						foundLine.isMatch = true
-						foundLine.conditionMatches = append(foundLine.conditionMatches, ipObject)
+						lineObj.isMatch = true
+						lineObj.conditionMatches = append(lineObj.conditionMatches, ipObject)
+						matchedLines = append(matchedLines, lineObj)
+
 					}
 				case args.Contains:
 					log.Debug("comparing %v %v for Contains", ipObject, args.Ipaddr)
 					if ipObject.Contains(args.Ipaddr) {
 						log.Debug("found contains match %v", ipObject)
-						foundLine.isMatch = true
+						lineObj.isMatch = true
+						lineObj.conditionMatches = append(lineObj.conditionMatches, ipObject)
+						matchedLines = append(matchedLines, lineObj)
 
-						foundLine.conditionMatches = append(foundLine.conditionMatches, ipObject)
 					}
 				case args.Subnet:
 					log.Debug("comparing %v %v for Subnet", ipObject, args.Ipaddr)
 					if args.Ipaddr.Contains(ipObject) {
 						log.Debug("found subnet match %v", ipObject)
-						foundLine.isMatch = true
+						lineObj.isMatch = true
+						lineObj.conditionMatches = append(lineObj.conditionMatches, ipObject)
+						matchedLines = append(matchedLines, lineObj)
 
-						foundLine.conditionMatches = append(foundLine.conditionMatches, ipObject)
 					}
 				}
 			} // for each ipMatch
 		} // for filename.Scanner
-		log.Print("all matched lines")
-		log.Printf("%+v", foundLines) // just prints a bunch of pointers.
-		for _, item := range foundLines {
-			log.Printf("  %+v", item)
+
+		switch {
+		case args.Json:
+			// TODO this prints all lines, not matching lines
+
+			b, err := json.MarshalIndent(matchedLines, "", "  ")
+			if err != nil {
+				log.Error(err)
+			}
+			fmt.Println(string(b))
+		case args.Trie:
+			log.Info("trie not supported")
+		default:
+			for _, line := range foundLines {
+				if lineObj.isMatch {
+					fmt.Printf("%v:%v:%v\n", line.Filename, line.Idx, line.Line)
+				}
+			}
 		}
 
 	} // for fileName range inputFiles
 	return nil
 } // func ipcmd
-
-// func doReports(matchingLines []*foundLine, args cliArgStruct, w io.Writer, lsm int) error {
-// 	newML := []*readLineExternal{}
-// 	newInternalML := []*foundLine{}
-
-// 	// have a filter on matchingLines which looks for LSM matches
-// 	// TODO
-
-// 	if args.Longest {
-// 		for _, tmp := range matchingLines {
-// 			for _, m := range tmp.ConditionMatches {
-// 				if ipaddr.NewIPAddressString(m).GetAddress().GetPrefixLen().Len() == lsm {
-// 					newInternalML = append(newInternalML, tmp)
-// 				}
-// 			}
-// 		}
-// 	}
-
-// 	if len(newInternalML) > 0 {
-// 		matchingLines = newInternalML
-// 	}
-
-// 	if !args.Debug {
-// 		for _, tmp := range matchingLines {
-// 			newML = append(newML, &readLineExternal{Idx: tmp.Idx, Filename: tmp.Filename, Line: tmp.Line})
-// 		}
-// 	}
-
-// 	var b []byte
-// 	var err error
-// 	switch {
-// 	case args.Json:
-// 		if args.Debug {
-// 			b, err = json.MarshalIndent(matchingLines, "", "  ")
-// 			if err != nil {
-// 				return err
-// 			}
-// 			fmt.Fprint(w, string(b))
-// 			fmt.Fprint(w, "\n")
-// 		} else {
-// 			b, err = json.MarshalIndent(newML, "", "  ")
-// 			if err != nil {
-// 				return err
-// 			}
-// 			fmt.Fprint(w, string(b))
-// 			fmt.Fprint(w, "\n")
-// 		}
-
-// 	case args.Trie:
-// 		//  also need tries for LPM I think.
-// 		IPv4Trie, IPv6Trie := getIPTries(args, matchingLines)
-// 		if IPv4Trie.Size() > 0 {
-// 			fmt.Println(matchingLines[0].Filename)
-// 			fmt.Println(IPv4Trie)
-// 		}
-// 		if IPv6Trie.Size() > 0 {
-// 			fmt.Println(matchingLines[0].Filename)
-// 			fmt.Println(IPv6Trie)
-// 		}
-// 	default:
-// 		log.Debug("need to log text")
-// 		for _, fLine := range matchingLines {
-// 			if fLine.IsMatch {
-// 				fmt.Fprintf(w, "%v:%v:%v\n", fLine.Filename, fLine.Idx, fLine.Line)
-// 			}
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func getIPTries(args cliArgStruct, matchingLines []*foundLine) (ipaddr.IPv4AddressTrie, ipaddr.IPv6AddressTrie) {
-// 	IPv4Trie := ipaddr.IPv4AddressTrie{}
-// 	IPv6Trie := ipaddr.IPv6AddressTrie{}
-
-// 	switch args.addressFamily {
-// 	case 4:
-// 		for _, match := range matchingLines {
-// 			for _, line := range match.conditionMatches {
-// 				IPv4Trie.Add(ipaddr.NewIPAddressString(line).GetAddress().ToIPv4())
-// 			}
-// 		}
-
-// 	case 6:
-// 		for _, match := range matchingLines {
-// 			for _, line := range match.conditionMatches {
-// 				IPv6Trie.Add(ipaddr.NewIPAddressString(line).GetAddress().ToIPv6())
-// 			}
-// 		}
-// 	}
-// 	return IPv4Trie, IPv6Trie
-
-// }
-
-// func getMatchingLines(args cliArgStruct, f inputFile) ([]*foundLine, int) {
-
-// 	fLines, err := readSingleFile(args, f)
-// 	if err != nil {
-// 		log.Fatal("error opening %v", f)
-// 	}
-// 	log.Debug("Read in %+v from %v", fLines, f.Filename)
-// 	var lsm int
-// 	// at this point fLines is []*readLine, for each line in the file I just read
-
-// 	for _, fLine := range fLines {
-// 		switch {
-
-// 		case args.Exact:
-// 			//log.Print("need to match exactly")
-// 			//log.Printf("working on line %v", fLine)
-// 			for _, ip := range fLine.ipRegexMatches {
-// 				ipObj := ipaddr.NewIPAddressString(ip).GetAddress()
-// 				//fmt.Printf("comparing %v %v\n", args.Ipaddr, ipObj)
-// 				if ipObj.Equal(args.Ipaddr) {
-// 					fLine.isMatch = true
-// 					fLine.conditionMatches = append(fLine.conditionMatches, ip)
-// 				}
-// 			}
-// 		case args.Subnet:
-// 			log.Debug("need to match subnet")
-// 			//log.Printf("working on line %v", fLine)
-// 			for _, ip := range fLine.ipRegexMatches {
-// 				ipObj := ipaddr.NewIPAddressString(ip).GetAddress()
-// 				// if ipObj.GetPrefixLen().Len() > 21 && ipObj.GetPrefixLen().Len() < 27 {
-// 				// 	continue
-// 				// }
-// 				if args.Ipaddr.Contains(ipObj) {
-// 					fLine.isMatch = true
-// 					fLine.conditionMatches = append(fLine.conditionMatches, ip)
-// 				}
-// 			}
-// 		case args.Contains:
-// 			log.Debug("need to match contains")
-// 			//log.Debugf("working on line %v", fLine)
-// 			for _, ip := range fLine.ipRegexMatches {
-// 				ipObj := ipaddr.NewIPAddressString(ip).GetAddress()
-// 				if ipObj.Contains(args.Ipaddr) {
-// 					fLine.isMatch = true
-// 					fLine.conditionMatches = append(fLine.conditionMatches, ip)
-// 				}
-// 			}
-
-// 			//	log.Fatal("LSM not supported yet")
-// 			// dump it all in a trie and get
-// 			/*
-// 				target := ipaddr.NewIPAddressString("192.168.1.150").GetAddress().ToIPv4()
-// 				match := trie.LongestPrefixMatch(target)
-// 			*/
-// 		}
-// 	}
-// 	if args.Longest {
-// 		// this works but what I think I really want is to populate matchingLines
-// 		// with things with the matching ip address length.
-// 		Ipv4Trie, Ipv6Trie := getIPTries(args, fLines)
-
-// 		if args.IsIPv4 && Ipv4Trie.Size() > 0 {
-// 			//fmt.Println("IPv4LSM", Ipv4Trie.LongestPrefixMatch(args.Ipaddr.ToIPv4()).GetPrefixLen().Len())
-// 			lsm = max(lsm, Ipv4Trie.LongestPrefixMatch(args.Ipaddr.ToIPv4()).GetPrefixLen().Len())
-// 		}
-
-// 		if args.IsIPv6 && Ipv6Trie.Size() > 0 {
-// 			//fmt.Println("IPv6LSM", Ipv6Trie.LongestPrefixMatch(args.Ipaddr.ToIPv6()).GetPrefixLen().Len())
-// 			lsm = max(lsm, Ipv6Trie.LongestPrefixMatch(args.Ipaddr.ToIPv6()).GetPrefixLen().Len())
-// 		}
-
-// 	}
-
-// 	var matchingLines = []*foundLine{}
-// 	for _, fLine := range fLines {
-// 		if fLine.isMatch {
-// 			matchingLines = append(matchingLines, fLine)
-// 		}
-// 	}
-
-// 	return matchingLines, lsm
-
-// }
-
-// func readSingleFile(args cliArgStruct, fileName inputFile) ([]*foundLine, error) {
-
-// 	if fileName.IsStdin {
-// 		fileName.Scanner = bufio.NewScanner(os.Stdin)
-// 	} else {
-// 		ifh, err := os.Open(fileName.Filename)
-// 		if err != nil {
-// 			log.Fatalf("error opening %v\n", fileName.Filename)
-// 		}
-// 		defer ifh.Close()
-
-// 		fileName.Scanner = bufio.NewScanner(ifh)
-// 	}
-
-// 	log.Debug("Need to read in", "file", fileName)
-
-// 	// now process the file
-
-// 	var idx = 0
-// 	var linesInFile = []*foundLine{}
-// 	for fileName.Scanner.Scan() {
-// 		idx++
-// 		line := fileName.Scanner.Text()
-// 		rl := foundLine{Idx: idx, Line: line, Filename: fileName.Filename}
-
-// 		if len(line) == 0 {
-// 			continue // optimization for blank lines.
-// 		}
-
-// 		// now find all ipv4 regex matches and ipv6 regex matches from the line
-// 		if args.IsIPv4 {
-// 			rl.ipRegexMatches = get_ipv4_addresses_from_line(rl.Line, args.IPv4Regex)
-// 		} else {
-// 			rl.ipRegexMatches = get_ipv6_addresses_from_line(rl.Line, args.IPv4Regex)
-// 		}
-
-// 		linesInFile = append(linesInFile, &rl)
-// 	}
-// 	return linesInFile, nil
-// }
 
 func get_ip_addresses_from_line(ipre *regexp.Regexp, addressFamily int, line string) []string {
 	var ret []string
@@ -390,23 +195,6 @@ func get_ip_addresses_from_line(ipre *regexp.Regexp, addressFamily int, line str
 	}
 	return ret
 }
-
-// func get_ipv4_addresses_from_line(line string, ipv4Regex *regexp.Regexp) []string {
-// 	return get_ip_addresses_from_line(ipv4Regex, line)
-// }
-
-// func get_ipv6_addresses_from_line(line string, ipv6Regex *regexp.Regexp) []string {
-
-// 	// hack because the regex is getting messy but this seems ok.
-// 	ret := []string{}
-// 	for _, m := range get_ip_addresses_from_line(ipv6Regex, line) {
-// 		if strings.Contains(m, ":") {
-// 			ret = append(ret, m)
-// 		}
-// 	}
-// 	return ret
-
-// }
 
 func getFilesFromArgs(inputFiles []string) ([]string, error) {
 	var ret []string
